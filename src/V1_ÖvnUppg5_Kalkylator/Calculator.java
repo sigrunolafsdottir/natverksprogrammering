33faa0cdb7ffb7214955f14a6f73037284009dc9
sigrunolafsdottir <solafsdottir@gmail.com>
2026-03-20 14:37:08 +0100

    Cleanup calculator operator check

--- src/V1_ÖvnUppg5_Kalkylator/Calculator.java
+++ src/V1_ÖvnUppg5_Kalkylator/Calculator.java
@@ -4,11 +4,6 @@ import java.util.List;
 
 public class Calculator {
 
-    private List<String> operatorList = List.of("+", "-", "*", "/");
-
-    protected boolean operatorSupported(String operator) {
-        return operatorList.contains(operator);
-    }
 
     protected boolean checkDivisionByZero(double b) {
         if (b == 0) {
@@ -19,9 +14,6 @@ public class Calculator {
 
     public double calculate(double a, double b, String operator)
             throws OperatorNotSupportedException, DivisionByZeroException {
-        if (!operatorSupported(operator)) {
-            throw new OperatorNotSupportedException("Felaktig operator " + operator);
-        }
 
         switch (operator) {
             case "+": return a + b;
@@ -34,7 +26,7 @@ public class Calculator {
                 return a / b;
             }
             default:
-                throw new DivisionByZeroException("Felaktig operator " + operator);
+                throw new OperatorNotSupportedException("Felaktig operator " + operator);
         }
     }
 
